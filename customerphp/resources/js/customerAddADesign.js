630f6d7fa88e79d3c69d4cd9da17e7b0a7f39b61
daryllwendel <feeeeez16@gmail.com>
2025-04-14 23:10:47 +0800

    laravel nani yawar

--- customerphp/resources/js/customerAddADesign.js
+++
@@ -1,6 +0,0 @@
-let defaultTshirt = document.getElementById("default-tshirt");
-let inputFile = document.getElementById("input-file");
-
-inputFile.onchange = function (){
-    defaultTshirt.src = URL.createObjectURL(inputFile.files[0]);
-}
